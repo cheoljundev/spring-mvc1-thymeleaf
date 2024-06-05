07c87ba4ec1713f4fca260b1774d6a560a16fa72
cheoljundev <cheoljundev@gmail.com>
2024-06-05 13:22:48 +0900

    URL link

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -67,9 +67,19 @@ public class BasicController {
         }
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+
+    }
+
+
     @GetMapping("/date")
     public String date(Model model) {
-        model.addAttribute("localDateTime", LocalDateTime.now() );
+
+        model.addAttribute("localDateTime", LocalDateTime.now());
         return "basic/date";
     }
 
