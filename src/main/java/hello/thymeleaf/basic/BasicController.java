c7a83c3757081f44919ad9bb517373b3cd5bd461
cheoljundev <cheoljundev@gmail.com>
2024-06-05 14:58:27 +0900

    블록 위치 수정

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -123,16 +123,16 @@ public class BasicController {
         return "basic/condition";
     }
 
-    @GetMapping("/block")
-    public String block(Model model) {
-        addUsers(model);
-        return "basic/block";
-    }
-
     @GetMapping("/comments")
     public String comments(Model model){
         model.addAttribute("data", "Spring!");
         return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
     }
 
     @Data
